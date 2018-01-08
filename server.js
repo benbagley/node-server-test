8435dd553198c09f52642ebc58040781d4f9b7e6
Ben Bagley <ben@benbagley.co.uk>
2018-01-08 16:15:22 +0000

    added projects

--- server.js
+++ server.js
@@ -39,13 +39,19 @@ hbs.registerHelper('screamIt', (text) => {
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'HomePage',
-    welcomeMessage: 'Welcome',
+    welcomeMessage: 'Welcome'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
